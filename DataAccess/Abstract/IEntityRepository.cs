6e5f8d0fd1468849f363918e12ab150a179ae7e2
berkant26 <youbeko26@gmail.com>
2024-08-02 15:43:11 +0300

    project is completed

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint
+    // 
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    { // T nin alabilecegi degerleri sinirlandirkdik referans tip ve IEntity'i implement eden siniflar olacak, Ientity de olmayacak
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T,bool>> filter);
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,70 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product> {
+                new Product{ProductId=1,CategoryId=1,ProductName="evidance",UnitPrice=15},
+                new Product{ProductId=2,CategoryId=2,ProductName="glass",UnitPrice=11},
+                new Product{ProductId=3,CategoryId=3,ProductName="iron",UnitPrice=41},
+                new Product{ProductId=4,CategoryId=4,ProductName=" chicken",UnitPrice=32},
+                new Product{ProductId=5,CategoryId=5,ProductName="lemon",UnitPrice=123},
+
+
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            //referans tipli veriyi direkt silemessin. referansini yakalaman lazim
+            Product productToDelete = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            _products.Remove(productToDelete);
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductId = product.ProductId;
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+
+
+        }
+    }
+}
